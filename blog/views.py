239458c481bcd40cfd258ab81746df4ffb086535
Ykyki4 <jimmypantalones@gmail.com>
2022-10-24 11:32:44 +0300

    replace sorted with order by

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-def get_likes_count(post):
-    return post.likes_count
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -32,9 +29,8 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(likes_count=Count('likes'))
-    popular_posts = sorted(posts, key=get_likes_count)
-    most_popular_posts = popular_posts[-5:]
+    posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
+    most_popular_posts = posts[0:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
